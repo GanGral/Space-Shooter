b699734cad7368558e87ae0186fb8af107814712
Igor Korablev <Ikorablev@osisoft.com>
2021-06-23 16:24:17 -0400

    6/23/2021 Enemy Waves objects implemented

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+    [SerializeField] int startingWave = 0;
+    [SerializeField] bool looping = false;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        do
+        {
+            yield return StartCoroutine(SpawnAllWaves());
+        }
+        while (looping);
+
+
+    }
+
+    private IEnumerator SpawnAllWaves()
+    {
+        for(int waveIndex = startingWave; waveIndex<waveConfigs.Count; waveIndex++)
+        {
+            WaveConfig currentWave = waveConfigs[waveIndex];
+            yield return StartCoroutine(SpawnAllEnemiesInAWave(currentWave));
+        }
+    }
+
+    private IEnumerator SpawnAllEnemiesInAWave(WaveConfig waveConfig)
+    {
+        for (int i = 0;  i <= waveConfig.GetNumberOfEnemies(); i++)
+        {
+            var newEnemy = Instantiate(waveConfig.GetEnemyPrefab(), waveConfig.GetWaypoints()[0].transform.position, Quaternion.identity);
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+    }
+
+
+}
